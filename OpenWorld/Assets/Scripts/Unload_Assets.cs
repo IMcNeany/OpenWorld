749d3d0f98b01d89375e1e09c2e3c65449271ffb
Isobel McNeany <icmc112@yahoo.co.uk>
2018-11-16 21:40:36 +0000

    Added in loading and unloading sections

---
+++ OpenWorld/Assets/Scripts/Unload_Assets.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unload_Assets : MonoBehaviour {
+    public string SectionToUnload;
+    GameObject LoadedSection;
+    public GameObject LoadingObject;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        StartCoroutine("UnLoad");
+    }
+
+    IEnumerator UnLoad()
+    {
+        //string Find.name(SectionToUnload.ToString());
+        LoadedSection = GameObject.Find(SectionToUnload) ;
+        Destroy(LoadedSection);
+        LoadingObject.SetActive(true);
+        gameObject.SetActive(false);
+        yield break;
+    }
+}
